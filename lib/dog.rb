f852022f9a8ed9b5427e3baf38cdee12fa3cf0dd
Judy Mwai <juddiemukami@gmail.com>
2022-08-18 13:35:23 +0300

    commit tree 72cdd7883a1cda604c10d88ab35da0b139585e3e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+    #setter method
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    #getter method
+    def name
+        #aceess the data from @this_dogs_name instance variable and return it
+        @this_dogs_name
+    end
 
 end
+Lassie = Dog.new
+Lassie.name= "Lassie"
+puts Lassie.name
